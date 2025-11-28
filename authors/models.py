7a51c76701cfd3e21c58fc2f9ed6c27114619584
artem <senteronter@gmail.com>
2025-11-28 12:36:25 +0300

    Added minimal template for django

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    full_name = models.CharField(max_length=128)
+    affiliation = models.CharField(max_length=256)
+    email = models.EmailField(blank=True, null=True)
+    citation_count = models.PositiveIntegerField(default=0)
+    h_index = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
